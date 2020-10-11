fb1c24e90b8e2fa69e1bc8e7ea2b4a3ffc96ef00
emmschuster <emmschuster@tsn.at>
2020-10-11 16:03:08 +0200

    update SpezificationBook and created database

--- Webstuff/myScript.js
+++ Webstuff/myScript.js
@@ -1,3 +1,25 @@
 function myFunction() {
-	<button onclick="window.print()">Print this page</button>
+	alert("Hello");
+}
+
+function scrollbaar() {
+ 	/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
 }
