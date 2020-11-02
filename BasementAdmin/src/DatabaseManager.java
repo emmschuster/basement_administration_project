2aafe5a4ca8fd3e1ef64dfe9c4bc649305f68cfa
emmschuster <emmschuster@tsn.at>
2020-11-02 17:21:43 +0100

    db connection done; testing

--- BasementAdmin/src/DatabaseManager.java
+++ BasementAdmin/src/DatabaseManager.java
@@ -10,8 +10,8 @@ import java.util.Scanner;
 
 public class DatabaseManager {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        //getConnection();
-        //ausfueren();
+        getConnection();
+        ausfueren();
     }
     static Connection con = null;
     public static Connection getConnection()
@@ -23,32 +23,38 @@ public class DatabaseManager {
             e.printStackTrace();
         }
 
-        Class.forName("mysql-connector-java-8.0.21");       //da irgendwie fehler
-        while (scan.hasNext()) {
-        con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/artikeldbschule1?serverTimezone=UTC&useSSL=false",  // DB
-                "root",                                 // User
-                scan.nextLine()                        // Passwort
-        );
-        scan.close();
+        //Class.forName("com.mysql.cj.jdbc");       //da irgendwie fehler
+        if (scan.hasNext()) {
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/kellerverw?serverTimezone=UTC&useSSL=false",  // DB
+                    "root",                                 // User
+                    scan.nextLine()                        // Passwort
+            );
+        //scan.close();
         }
         return con;
 
     }
 
     private static void ausfueren() {
-        try {
-            //Class.forName("org.sqlite.JDBC");
-            Statement stmt = con.createStatement();
-            String name;
-            try (ResultSet rs0 = stmt.executeQuery(" SELECT name FROM rezept;")) {
-                while (rs0.next()) {
-                    name = rs0.getString("Name vom Rezept");
-                }
+        try (Statement stmt = con.createStatement()) {
+            String name,rname,zname,fmenge,zeinheit;
+            ResultSet rs0 = stmt.executeQuery(" SELECT name FROM rezept;");
+            while (rs0.next()) {
+                name = rs0.getString("name");
+                System.out.println(name);
             }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
+            ResultSet rs1=stmt.executeQuery("SELECT r.name, z.name, f.menge, z.einheit from Rezept r " +
+                    "join fusion f on f.RezID = r.RezID join Zutat z on f.ZutID = z.ZutID where r.RezID = 1;");
+            while (rs1.next()) {
+                rname = rs1.getString("r.name");
+                zname = rs1.getString("z.name");
+                fmenge = rs1.getString("f.menge");
+                zeinheit = rs1.getString("z.einheit");
+                System.out.println(rname +"||"+zname+"||"+fmenge+"||"+zeinheit);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
